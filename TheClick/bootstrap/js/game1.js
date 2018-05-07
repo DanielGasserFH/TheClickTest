172d07da89a17207f0e40addbf337c7b29e12a91
lukasgundolf <l.g.1@gmx.at>
2018-05-07 14:00:02 +0200

    game 1 & 2 --> code kommentiert

--- TheClick/bootstrap/js/game1.js
+++ TheClick/bootstrap/js/game1.js
@@ -1,28 +1,28 @@
-var myTestElement = document.getElementById("test");
+var myTestElement = document.getElementById("test");    //mit document.getElementById wird auf das gewünschte Element in der HTML datei zugegriffen
 var anzahl = 0;
 myTestElement.innerHTML= anzahl;
-var seconds = 10;
+var seconds = 10;                               // hier werden die variablen erzeugt, die zum spielen nötig sind
 var counting = true;
 
 var timerInterval = null;
 
-function startGame(){
+function startGame(){                           //mit dieser methode wird das spiel gestartet, in dem ein neuer intervall erzeugt wird (sofern es noch keinen gibt)
     if(timerInterval == null) {
         timerInterval = setInterval(timer,1000);
     }
 }
-function resetGame() {
+function resetGame() {                          //mit dieser methode wird das spiel zurückgesetzt, in dem die seconds wieder auf 10 gesetzt und die anzahl auf 0 gesetzt wird
     seconds = 10;
     anzahl = 0;
     counting = true;
 }
 
 
-function timer(){
+function timer(){                               //mit dieser methode wird erreicht, dass ein benutzer nach ablauf der zeit nicht weiter klicken kann, in dem die variable counting auf false gesetzt wird
     seconds--;
     document.getElementById("counter").innerHTML = seconds + "s"
     if (seconds <= 0) {
-        document.getElementById("counter").innerHTML = "Zeit um";
+        document.getElementById("counter").innerHTML = "Zeit um"; // wenn der intervall abgelaufen ist wird "zeit um" angezeigt
         // Anzahl speichern
         counting=false;
         clearInterval(timerInterval);
@@ -34,11 +34,11 @@ function timer(){
 
 
 
-var myButton = document.getElementById("counter");
+var myButton = document.getElementById("counter");          //das ist die eigentliche "spielmethode", hier wird (sofern counting true ist) die anzahl pro klick um 1 erhöht
     myButton.onclick = function () {
         if(counting==true) {
             anzahl++;
-            myTestElement.innerHTML = anzahl;
+            myTestElement.innerHTML = anzahl;               //hier wird die variable anzahl am bildschirm angezeigt
         }
 }
 
--- TheClick/bootstrap/js/game2.js
+++ TheClick/bootstrap/js/game2.js
@@ -1,4 +1,4 @@
-var myTestElement = document.getElementById("test");
+var myTestElement = document.getElementById("test");            //mit document.getElementById wird auf das gewünschte Element in der HTML datei zugegriffen
 var anzahl = 0;
 myTestElement.innerHTML= anzahl;
 var seconds = 25;
@@ -11,13 +11,14 @@ var button = $('#counter');
 var game2Started = false;
 
 
-function newPosition() {
+function newPosition() {                                        // diese methode teilt dem button eine neue position zu und sorgt dadurch für die bewegung des buttons
     //1. die groesse des gameboards feststellen
 
-    var gameboardWidth = gameboard.width() - 10 - button.width();
+    var gameboardWidth = gameboard.width() - 10 - button.width();   //-10 -buttonwidth , damit der button nicht aus dem container raussteht
     var gameboardHeight = gameboard.height() - 10 - button.height();
 
     //2. zwei werte (x und y) im bereich von 0 bis gameboardbreite bzw. gameboardhoehe erzeugen
+
     var newX = Math.floor(Math.random() * gameboardWidth);
     var newY = Math.floor(Math.random() * gameboardHeight);
 
@@ -29,13 +30,13 @@ function newPosition() {
     });
 }
 
-function startGame(){
+function startGame(){                               //diese methode startet das spiel, indem ein ein intervall erzeugt wird (sofern noch keines existiert)
     if(timerInterval == null) {
-        timerInterval = setInterval(timer,1000);
+        timerInterval = setInterval(timer,1000);    //setzt die variable game2started auf true um spielen zu können.
     }
     game2Started = true;
 }
-function resetGame() {
+function resetGame() {                             //diese Methode startet das spiel neu indem die variable seconds wieder auf 25 gesetzt wird und sie anzahl zurückgesetzt wird
     seconds = 25;
     anzahl = 0;
     game2Started = false;
@@ -44,23 +45,23 @@ function resetGame() {
 
 function timer(){
     seconds--;
-    document.getElementById("counter").innerHTML = seconds + "s"
+    document.getElementById("counter").innerHTML = seconds + "s"            //stellt den timer auf dem button dar
     if (seconds <= 0) {
-        document.getElementById("counter").innerHTML = "Zeit um";
+        document.getElementById("counter").innerHTML = "Zeit um";           //wenn seconds 0 ist steht auf dem button zeit um.
         // Anzahl speichern
         game2Started=false;
-        clearInterval(timerInterval);
+        clearInterval(timerInterval);                                       //ausserdem wird das intervall gelöscht und die game2started wieder auf false gesetzt, dass man nicht mehr klicken kann
         timerInterval = null;
     }
 }
 
 
 var myButton = document.getElementById("counter");
-myButton.onclick = function () {
+myButton.onclick = function () {                                            //diese methode regelt das raufzählen der variable anzahl pro klick und sorgt dafür, dass die anzahl als score dargestellt wird
     if(game2Started) {
         anzahl++;
         myTestElement.innerHTML = 'Score: ' + anzahl;
-        newPosition();
+        newPosition();                                                      //hier wird dem button eine neue position zugeschrieben in dem die oben beschriebene methode newPosition() aufgerufen wird
 
     }
 
